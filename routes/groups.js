3a657efc1ede1aaee512c8df6fe3ec5f63d2e959
PabloValles <valles.pablo@hotmail.com>
2022-04-19 16:30:42 -0300

    finalizada subida de archivos

--- routes/groups.js
+++ routes/groups.js
@@ -1,17 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/groupsController');
+const multer = require("multer");
+const path = require("path");
+const controller = require("../controllers/groupsController");
+
+// Donde vamos a guardar los archivos
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../public/img/groups"));
+  },
+  filename: (req, file, cb) => {
+    // path.basename(file.originalname) -> nombre del archivo
+    // path.extname(file.originalname) -> nombre de la extension del archivo
+    // file.filename -> nombre del archivo
+
+    const newFileName = `group-${Date.now()}.${path.extname(
+      file.originalname
+    )}`;
+    cb(null, newFileName);
+  },
+});
+
+// Ejecutar la configuración de Multer
+const upload = multer({ storage: storage });
 
 // Todos los grupos
-router.get('/', controller.index);
+router.get("/", controller.index);
 
 // Formulario de creación
-router.get('/create', controller.create);
+router.get("/create", controller.create);
 
 // Procesamiento del formulario de creación
-router.post('/', controller.store);
+router.post("/", upload.single("group-image"), controller.store);
 
 // Detalle de un grupo
-router.get('/:id', controller.show);
+router.get("/:id", controller.show);
 
 module.exports = router;
